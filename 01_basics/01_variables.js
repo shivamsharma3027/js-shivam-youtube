99419a667a34675579a5f6069ea88a416123027c
shivamsharma3027 <75126801+shivamsharma3027@users.noreply.github.com>
2023-11-27 09:57:11 +0000

    variables,datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=12//not changeable
+let accountEmail="shivam@123.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=34;//not allowed
+// console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+// prefere not use in var bcoz of functional scope and block scope
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+//alert("hello");//run only on web browser so we are usinf node js
+
+let state=null;//empty
+let age=18
+let isLoggedIn=false
+//number => 2 to power 53
+//big int
+//String=>"",''
+//Boolean=>true,false
+//null--->stand alone value or empty
+//undefined=>value not assigned
+//symbol=>unique 
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
